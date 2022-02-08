a9440030a333779e5571a8ccb49cea0649b0190a
Vance McGrady <vancemcgrady@gmail.com>
2022-02-08 11:10:03 -0700

    turn color change functionality working

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 const gameDisplay = (() => {
   const squaresArray = ["a", "b", "c", "d", "e", "f", "g", "h", "i"];
   const gameContainer = document.querySelector(".game-container");
+  const playerCardX = document.querySelector("#player-x");
+  const playerCardO = document.querySelector("#player-o");
 
   squaresArray.forEach(
     (item) =>
-      (gameContainer.innerHTML += ` <div id="${item}" class="field"></div>`)
+      (gameContainer.innerHTML += ` <div id="${item}" class="field">${item}</div>`)
   );
+
+  return { squaresArray, gameContainer, playerCardX, playerCardO };
 })();
 
 class Player {
@@ -20,14 +24,19 @@ class Player {
 
 const gameController = (() => {
   let whosTurnIsIt = true;
+
   function changeTurn() {
     gameController.whosTurnIsIt = !gameController.whosTurnIsIt;
+
+    gameDisplay.playerCardX.classList.toggle("turn-highlight");
+    gameDisplay.playerCardO.classList.toggle("turn-highlight");
   }
   return { whosTurnIsIt, changeTurn };
 })();
 
 (function eventListeners() {
   const squares = document.querySelectorAll(".field");
+
   squares.forEach((square) =>
     square.addEventListener(
       "click",
